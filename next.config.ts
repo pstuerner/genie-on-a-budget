abc4703ab2e041fed5753f198353dd83dd6e7d39
Philipp Stuerner <philipp.stuerner@web.de>
2026-02-02 20:28:47 +0100

    add data

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatar.vercel.sh",
+      }
+    ],
+  },
+};
+
+export default nextConfig;
